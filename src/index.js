7486964371ebc3fa07e0c4da78870680226ef8d0
Kochetov Roman <k04erg0@gmail.com>
2021-05-22 15:55:34 +0300

    добавил кнопки на главную

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+
+import './js/bb-def'
+
+
+
+
+
+
+const bvbBtn = document.querySelector('[data="bvb"]')
+const vsOpenBtn = document.querySelector('[data="vs-open"]')
+const vs3betBtn = document.querySelector('[data="vs3bet"]')
+const pushFoldBtn = document.querySelector('[data="push-fold"]')
+const sqzBtn = document.querySelector('[data="sqz"]')
+const rfiBtn = document.querySelector('[data="rfi"]')
+
+
+
---
+++ src/js/bb-def.js
@@ -0,0 +1,4 @@
+// import bbTemplate from './template/bb.hbs';
+
+// const bbDefBtn = document.querySelector('[data="bb-def"]')
+// // console.log(bbDefBtn)
